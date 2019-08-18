f3e84f911231c6cc2be5170b8fe6cdcc3ff26c22
Pravar Agrawal <pravarag@gmail.com>
2019-08-18 19:47:04 +0530

    [lsbaws-part-1] add readme.md

---
+++ lsbaws-part-1/README.md
@@ -0,0 +1,2 @@
+This section involves my learning as part of [Let's build a web
+server](https://ruslanspivak.com/lsbaws-part1/)series
---
+++ lsbaws-part-1/simple-server.py
@@ -0,0 +1,21 @@
+import socket
+
+
+def web_server(host, port):
+    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listen_socket.bind((host, port))
+    listen_socket.listen(1)
+    print(f'Serving HTTP on port {port}')
+    while True:
+        client_connection, client_address = listen_socket.accept()
+        request_data = client_connection.recv(1024)
+        print(request_data.decode('utf-8'))
+        http_response = b"HTTP/1.1 200 OK"
+        client_connection.sendall(http_response)
+        client_connection.close()
+
+
+if __name__ == '__main__':
+    HOST, PORT = '', 8000
+    web_server(HOST, PORT)
